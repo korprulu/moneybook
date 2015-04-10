be4546dc08ec1f6842c4ec1b82d86f4157ab85f2
Kevin Chiu <korprulu9@gmail.com>
2015-04-10 18:40:03 +0800

    Modify AccountItem model

--- api/models/account.go
+++ api/models/account.go
@@ -1,46 +1,41 @@
 package models
 
 import (
-	"errors"
 	"time"
 
 	"appengine/datastore"
 )
 
 type AccountItem struct {
-	Owner       string        `json:"owner" datastore:"owner"`
-	DateTime    time.Time     `json:"dateTime" datastore:"datetime"`
-	Amount      int64         `json:"amount" datastore:"amount"`
-	Tags        []Tag         `json:"tags,emitempty" datastore:"tags"`
-	Description string        `json:"description" datastore:"description,noindex"`
-	SubItems    []AccountItem `json:"subItem,emitempty"`
-}
-
-type AccountSubItem struct {
-	Description string `json:"description,emitempty" datastore:"description,noindex"`
-	Amount      int64  `json:"amount" datastore:"amount"`
+	Owner       string         `json:"owner" datastore:"owner"`
+	DateTime    time.Time      `json:"dateTime" datastore:"datetime"`
+	Amount      int64          `json:"amount" datastore:"amount"`
+	Tags        []Tag          `json:"tags,emitempty" datastore:"tags"`
+	Description string         `json:"description,emitempty" datastore:"description,noindex"`
+	SubItems    []*AccountItem `json:"subItem,emitempty" datastore:"subitems,noindex"`
+	Links       []*AccountItem `json:"links,emitempty" datastore:"links,noindex"`
 }
 
 func (ai *AccountItem) Save(c chan<- datastore.Property) error {
 	defer close(c)
 
-	subItemLen := len(ai.SubItems)
+	// subItemLen := len(ai.SubItems)
 
-	if subItemLen == 0 {
-		return datastore.SaveStruct(ai, c)
-	}
+	// if subItemLen == 0 {
+	// 	return datastore.SaveStruct(ai, c)
+	// }
 
-	// Check total amount of sub item is equal to item's amount.
-	var total int64 = 0
+	// // Check total amount of sub item is equal to item's amount.
+	// var total int64 = 0
 
-	for i := 0; i < subItemLen; i++ {
-		item := ai.SubItems[i]
-		total += item.Amount
-	}
+	// for i := 0; i < subItemLen; i++ {
+	// 	item := ai.SubItems[i]
+	// 	total += item.Amount
+	// }
 
-	if total != ai.Amount {
-		return errors.New("Item amount is not equal to total amount of sub item.")
-	}
+	// if total != ai.Amount {
+	// 	return errors.New("Item amount is not equal to total amount of sub item.")
+	// }
 
 	return datastore.SaveStruct(ai, c)
 }
